94d2ebae56bc904461f77477812be46b7ff16817
Nayeema <neemi.oct27@gmail.com>
2020-02-16 19:02:59 -0500

    first commit

---
+++ src/test/java/stepDefinations/stepDefination.java
@@ -0,0 +1,41 @@
+package stepDefinations;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class stepDefination {
+
+    @Given("^User is on NetBanking landing page$")
+    public void user_is_on_netbanking_landing_page() throws Throwable {
+      System.out.println("navigated to login url");
+    }
+
+    @When("^User login into application with username and password$")
+    public void user_login_into_application_with_username_and_password() throws Throwable {
+    	System.out.println("Logged in success");
+    }
+
+    @Then("^Home page is populated$")
+    public void home_page_is_populated() throws Throwable {
+    	System.out.println("Validated home page");
+    }
+    
+    @When("^User login into application with \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void user_login_into_application_with_something_and_password_something(String strArg1, String strArg2) throws Throwable {
+       System.out.println(strArg1);
+       System.out.println(strArg2);
+    }
+    @Then("^Cards displayed are \"([^\"]*)\"$")
+    public void cards_displayed_are(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println(arg1);
+    }
+    
+
+}
